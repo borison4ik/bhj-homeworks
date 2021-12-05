a82dafaf8610cba7d52af7946fbc13aeaacad4d1
Valentin Borisenko <borison2004@yandex.ru>
2021-12-05 14:13:53 +0700

    resolved abs task

---
+++ dom/ads/task.js
@@ -0,0 +1,44 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', load);
+
+function load() {
+  const rotators = [...document.querySelectorAll('.rotator')];
+
+  rotators.forEach((element) => {
+    onStartRotate(element);
+  });
+}
+
+function onStartRotate(element) {
+  const phrases = [...element.children];
+  let current = phrases[0];
+
+  onChangePhrase(phrases, current);
+}
+
+function onChangePhrase(phrases, current) {
+  const speed = getSpeed(current);
+  const color = getColor(current);
+
+  setTimeout(() => {
+    current.classList.contains('rotator__case_active') &&
+      current.classList.remove('rotator__case_active');
+    current = current.nextElementSibling ? current.nextElementSibling : phrases[0];
+    current.classList.add('rotator__case_active');
+    onChangeColor(current, color);
+
+    onChangePhrase(phrases, current);
+  }, speed);
+}
+
+function onChangeColor(el, color) {
+  el.style.color = color ? color : '';
+}
+
+function getSpeed(el) {
+  return Number.parseInt(el.dataset.speed) && Math.floor(1 + Math.random() * 20) * 100;
+}
+
+function getColor(el) {
+  return el.dataset.color ? el.dataset.color : '#000000';
+}
